774e10ef184596f7c3523e6cf8ca90ffd3a6264e
BrownCow371 <jmbrowncow@gmail.com>
2019-01-13 12:16:11 -0800

    remove default behavior from app.js, add navbar and test components. import router module.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+//Components
+import NavBar from './NavBar.js';
+import Test from './Test.js';
+
+// import logo from './logo.svg';
+// import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="wrapper">
+          <NavBar />
+          <Route exact path='/test' component={Test} />
+        </div>
+      </Router>
     );
   }
 }
