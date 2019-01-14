e1ce041fb08d78f9272a7f77e21c54e70b986a6a
BrownCow371 <jmbrowncow@gmail.com>
2019-01-14 07:58:22 -0800

    updated actions to use .sendStatus. update create and update actions. add destroy action

--- server/controllers/job.ctrl.js
+++ server/controllers/job.ctrl.js
@@ -20,34 +20,20 @@ module.exports = {
         })
     },
     create: (request, result, next) => {
-        // result.send('Job Created Successfully')
-
         let {title, description, postingLink, dateApplied, applicationMethod} = request.body
-
         let newJob = new Job({title, description, postingLink, dateApplied, applicationMethod})
-        // result.send(newJob)
-        newJob.save(function (err){
+       
+        newJob.save(function (err, job){
             if (err){
-                return next(err);
-            }
-            result.send(newJob)
+                result.send(err);
+            } else if (!job) {
+                result.sendStatus(404)
+            } else {
+                job.addCompany(request.body.company_id).then((_job)=> {
+                    result.send(newJob)
+                })
+           } next()
         })
-        // saveJob({title, description, postingLink, dateApplied, applicationMethod})
-    
-        // function saveJob(obj){
-        //     new Job(obj).save((err, job) => {
-        //         if (err){
-        //             result.send(err)
-        //         } else if (!job){
-        //             result.send(400)
-        //         } else {
-        //             return job.addCompany(request.body.company_id).then((_job) => {
-        //                 return result.send(_job)
-        //             })
-        //         }
-        //         next()
-        //     })
-        // }
     },
     show: (request, result, next) => {
         Job.findById(request.params.id)
@@ -55,7 +41,7 @@ module.exports = {
             if(err){
                 result.send(err)
             } else if (!job){
-                result.send(404)
+                result.sendStatus(400)
             }else{
                 result.send(job)
             }next()
@@ -64,19 +50,32 @@ module.exports = {
     update: (request, result, next) => {
         let {title, description, postingLink, dateApplied, applicationMethod} = request.body
         
-        Job.findOneAndUpdate(
-            {id: request.params.id}, 
-            {title, description, postingLink, dateApplied, applicationMethod}
-        ).exec((err, job) =>{
+        Job.findByIdAndUpdate(
+            request.params.id, 
+            {title, description, postingLink, dateApplied, applicationMethod},
+            {new:true},
+            function(err, job){
+                if(err){
+                    result.send(err)
+                } else if (!job){
+                    result.sendStatus(404)
+                } else{
+                    result.send(job)
+                }next()
+            }
+        )
+    },
+    destroy: (request, result, next) => {
+        Job.findByIdAndDelete(request.params.id,
+        function(err, job){
             if(err){
                 result.send(err)
-            } else if (!job){
-                result.send(404)
-            }else{
-                result.send(job)
-            }next()
+            } else if (!job) {
+                result.sendStatus(404)
+            } else {
+                result.send(`Job id ${job.id} has been deleted`)
+            }
         })
-
     },
     noteJob: (request, result, next) => {
         Job.findById(request.params.id).then((job)=>{
@@ -84,7 +83,8 @@ module.exports = {
                 text: request.body.note,
                 entryDate: request.body.date
             }).then(()=>{
-                return result.json({msg: "Done"})
+                // return result.json({msg: "Done"})
+                result.send(job)
             })
         }).catch(next)
     }
