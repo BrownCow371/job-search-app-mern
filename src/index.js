ec3f452f144c339587e44dbec881ac507c5de9b0
BrownCow371 <jmbrowncow@gmail.com>
2019-01-21 14:08:37 -0800

    remove service worker and comment pertaining to service worker. import bootstrap

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap-theme.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
@@ -10,7 +12,7 @@ import './index.css';
 import App from './App';
 import appReducer from './reducers/appReducer.js';
 
-// import * as serviceWorker from './serviceWorker';
+// create store using appReducer and Thunk
 const store = createStore(appReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
@@ -20,7 +22,3 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-// serviceWorker.unregister();
