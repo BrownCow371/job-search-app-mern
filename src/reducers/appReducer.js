f4fc6edea6bf59e2279a7c6ee609b4b6531bbd8e
BrownCow371 <jmbrowncow@gmail.com>
2019-01-23 11:15:40 -0800

    add state and cases for loading data and fetching jobs to reducer

--- src/reducers/appReducer.js
+++ src/reducers/appReducer.js
@@ -1,5 +1,14 @@
-export default function appReducer(state, action){
+export default function appReducer(
+    state = {
+        loading: false,
+        jobs: []
+    }, 
+    action){
     switch(action.type){
+        case 'LOADING_DATA':
+            return {...state, loading: true}
+        case 'FETCH_JOBS':
+            return {...state, jobs: action.payload, loading: false}
         default:
             return state;
     }   
