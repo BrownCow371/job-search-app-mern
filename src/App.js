15c1a897c463df4d53de3d6f10be0bc997bc537a
BrownCow371 <jmbrowncow@gmail.com>
2019-02-11 14:03:20 -0800

    add antd layout and header. add routes for companies and companies/:id. add fetchCompanies to ComponentDidMount

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {Layout} from 'antd';
 
 //Components
 import MyNavBar from './components/static/NavBar.js';
@@ -7,35 +8,47 @@ import Test from './components/static/Test.js';
 import Greeting from './components/static/Greeting.js';
 import Jobs from './containers/jobs/Jobs.js';
 import ShowJob from './containers/jobs/ShowJob.js';
+import Companies from './containers/companies/Companies.js';
+import ShowCompany from './containers/companies/ShowCompany.js';
+
+
 import {connect} from 'react-redux';
 
 // actions
-import {fetchJobs} from './actions/jobs.js'
+import {fetchJobs} from './actions/jobs.js';
+import {fetchCompanies} from './actions/companies.js';
 
+const {Header} = Layout;
 class App extends Component {
 
   componentDidMount(){
     this.props.fetchJobs();
+    this.props.fetchCompanies();
   }
   
   render() {
     return (
       <Router>
-        <>
+        <Layout className ="layout">
           
           <div className="outside-wrapper">
-            <MyNavBar />
+            <Header>
+              <MyNavBar />
+            </Header>
             <div className="inside-wrapper">
               <Route path='/' component={Greeting} />
               <Route exact path='/jobs' component={Jobs} />
               <Route exact path='/jobs/:id' component={ShowJob} />
               <Route exact path='/test' component={Test} />
+              <Route exact path='/companies' component={Companies} />
+              <Route exact path='/companies/:id' component={ShowCompany} />
+
             </div>
           </div>
-        </>
+        </Layout>
       </Router>
     );
   }
 }
 
-export default connect(null, {fetchJobs})(App);
+export default connect(null, {fetchJobs, fetchCompanies})(App);
