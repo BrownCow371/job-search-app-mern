3104ae20b8aead2316e775524c81ad8c4c47dd77
BrownCow371 <jmbrowncow@gmail.com>
2019-02-12 12:23:27 -0800

    add addCompany action creator to companies actions file

--- src/actions/companies.js
+++ src/actions/companies.js
@@ -14,3 +14,19 @@ export function fetchCompanies(){
         .then(companies => dispatch({type: 'FETCH_COMPANIES', payload: companies}))
     }
 }
+
+export function addCompany(requestData){
+    return (dispatch) => {
+        dispatch({type: 'LOADING_DATA'});
+        return fetch(`/api/companies`, {
+            method: 'POST',
+            headers:{ 
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(requestData)
+        })
+        .then(response => response.json())
+        .then(company => dispatch({type: 'ADD_COMPANY', payload: company}))
+    }
+}
