5a6f81df81de9d38f24b28886b1314994fc73e70
BrownCow371 <jmbrowncow@gmail.com>
2019-02-12 12:26:46 -0800

    update NavBar to use sub-menus for Companies and Jobs. add new Company and new jobs under submenus

--- src/components/static/NavBar.js
+++ src/components/static/NavBar.js
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 
 
 const MyNavBar = () => {
+
+    const SubMenu = Menu.SubMenu;
       
     return (
         <Menu mode="horizontal" theme="dark">
@@ -14,12 +16,23 @@ const MyNavBar = () => {
             <Menu.Item key = "test">
                 <Link to ="/test" exact="true">TEST</Link>
             </Menu.Item>
-            <Menu.Item key ="jobs">
-                <Link to ="/jobs" exact="true">JOBS</Link>
-            </Menu.Item>
-            <Menu.Item key ="companies">
-                <Link to ="/companies" exact="true">COMPANIES</Link>
-            </Menu.Item>
+            <SubMenu title ="JOBS">
+                <Menu.Item key ="jobs">
+                    <Link to ="/jobs" exact="true">Jobs List</Link>
+                </Menu.Item>
+                <Menu.Item key ="jobs-new">
+                    <Link to ="/jobs/new" exact="true">New Job Form</Link>
+                </Menu.Item>
+            </SubMenu >
+            <SubMenu title ="COMPANIES">
+                <Menu.Item key="companies">
+                    <Link to ="/companies" exact="true">Companies List</Link>
+                </Menu.Item>
+                <Menu.Item key ="companies-new">
+                    <Link to ="/companies/new" exact="true">New Company Form</Link>
+                </Menu.Item>
+            </SubMenu >
+            
         </Menu> 
     )
 }
