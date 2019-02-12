118c4b991ed3317cf5eebe5dbd5dd2b4ca46dda6
BrownCow371 <jmbrowncow@gmail.com>
2019-02-12 12:25:30 -0800

    add renderthis() function to manage new vs :id routes. update render to use it.

--- src/components/companies/CompanyDetails.js
+++ src/components/companies/CompanyDetails.js
@@ -2,28 +2,37 @@ import React from 'react';
 import {Row, Col} from 'antd';
 
 const CompanyDetails =({company})=>{
+    // need renderThis so that if the route is "new" it doesn't try to render this component
 
-    return (
-        <div>
-            <Row>
-                <Col span={4}><strong>Name:</strong></Col>
-                <Col>{company.name}</Col>
-            </Row>
-            <Row>
-                <Col span={4}><strong>Contact: </strong></Col>
-                <Col>{company.contactPerson}</Col>
-            </Row>
-            <Row>
-                <Col span={4}><strong>Company Link: </strong></Col>
-                <Col><a href={company.companyLink} target="_blank" rel="noopener noreferrer">Click Here</a></Col>
-            </Row>
-            <Row>
-                <Col span={4}><strong>Notes: </strong></Col>
-                <Col>{company.notes}</Col>
-            </Row>
+    const renderThis = () => {
+        return !!company
+    }
 
-        </div>
-    )
+    if(renderThis()){
+        return (
+            <div>
+                <Row>
+                    <Col span={4}><strong>Name:</strong></Col>
+                    <Col>{company.name}</Col>
+                </Row>
+                <Row>
+                    <Col span={4}><strong>Contact: </strong></Col>
+                    <Col>{company.contactPerson}</Col>
+                </Row>
+                <Row>
+                    <Col span={4}><strong>Company Link: </strong></Col>
+                    <Col><a href={company.companyLink} target="_blank" rel="noopener noreferrer">Click Here</a></Col>
+                </Row>
+                <Row>
+                    <Col span={4}><strong>Notes: </strong></Col>
+                    <Col>{company.notes}</Col>
+                </Row>
+
+            </div>
+        )
+    } else {
+        return null;
+    }
 }
 
 export default CompanyDetails;
