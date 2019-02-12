0a9f4a5366e2aeb0000f89cbc945361519782aa5
BrownCow371 <jmbrowncow@gmail.com>
2019-02-12 12:28:46 -0800

    add render logic to redirect if no company is found and route id is not new.

--- src/containers/companies/ShowCompany.js
+++ src/containers/companies/ShowCompany.js
@@ -1,19 +1,23 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux'
-import CompanyDetails from '../../components/companies/CompanyDetails.js'
+import {connect} from 'react-redux';
+import {Redirect} from 'react-router-dom';
+import CompanyDetails from '../../components/companies/CompanyDetails.js';
 
 class ShowCompany extends Component {
 
     render(){
         let id = this.props.match.params.id;
         let company = this.props.companies.find(company => company._id === id)
-        return (
-            <div>
-                <CompanyDetails
-                    company={company}
-                />
-            </div>
-    )}
+        if (!company && this.props.match.params.id !=='new'){
+            return <Redirect to="/"/>
+        } else {
+            return (
+                    <CompanyDetails
+                        company={company}
+                    />
+            )
+        }
+    }
 }
 const mapStateToProps = (state) => {
     return {companies: state.companies}
