d7fdfa5ee32d30ccc636069d1bdcda34d62e727c
BrownCow371 <jmbrowncow@gmail.com>
2019-02-25 17:51:30 -0800

    update return to return null if job is not available

--- src/components/jobs/JobDetails.js
+++ src/components/jobs/JobDetails.js
@@ -3,41 +3,49 @@ import {Row, Col} from 'antd';
 import NotesList from './NotesList.js'
 
 const JobDetails =({job})=>{
+    // need renderThis so that if the route is "new" it doesn't try to render this component
 
-    let date = new Date(job.dateApplied);
-    return (
-        <div>
-            <Row>
-                <Col span={4}><strong>Title:</strong></Col>
-                <Col><a href={job.postingLink} target="_blank" rel="noopener noreferrer"> {job.title}</a></Col>
-            </Row>
-            <Row>
-                <Col span={4}><strong>Company Name: </strong></Col>
-                <Col>{job.company.name}</Col>
-            </Row>
-            <Row>
-                <Col span={4}><strong>Date Applied: </strong></Col>
-                <Col>{date.toLocaleDateString()}</Col>
-            </Row>
-            <Row>
-                <Col span={4}><strong>App Method: </strong></Col>
-                <Col>{job.applicationMethod}</Col>
-            </Row>
-            <Row>
-                <Col span={4}><strong>Description: </strong></Col>
-                <Col>{job.description}</Col>
-            </Row>
-            <Row>
-                <Col span={4}><strong>Notes: </strong></Col>
-            </Row>
-            <Row>
-                <NotesList
-                    notes = {job.notes}
-                />
-            </Row>
-            
-        </div>
-    )
+    const renderThis = () => {
+        return !!job
+    }
+
+    if (renderThis()){
+        let date = new Date(job.dateApplied);
+        return (
+            <div>
+                <Row>
+                    <Col span={4}><strong>Title:</strong></Col>
+                    <Col><a href={job.postingLink} target="_blank" rel="noopener noreferrer"> {job.title}</a></Col>
+                </Row>
+                <Row>
+                    <Col span={4}><strong>Company Name: </strong></Col>
+                    <Col>{job.company.name}</Col>
+                </Row>
+                <Row>
+                    <Col span={4}><strong>Date Applied: </strong></Col>
+                    <Col>{date.toLocaleDateString()}</Col>
+                </Row>
+                <Row>
+                    <Col span={4}><strong>App Method: </strong></Col>
+                    <Col>{job.applicationMethod}</Col>
+                </Row>
+                <Row>
+                    <Col span={4}><strong>Description: </strong></Col>
+                    <Col>{job.description}</Col>
+                </Row>
+                <Row>
+                    <Col span={4}><strong>Notes: </strong></Col>
+                </Row>
+                <Row>
+                    <NotesList
+                        notes = {job.notes}
+                    />
+                </Row>
+            </div>
+        )
+    }  else {
+        return null;
+    } 
 }
 
 export default JobDetails;
