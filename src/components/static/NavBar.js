8025b6c7024efb364a57eda463403626f63711b5
BrownCow371 <jmbrowncow@gmail.com>
2019-02-25 17:53:51 -0800

    remove link for test route

--- src/components/static/NavBar.js
+++ src/components/static/NavBar.js
@@ -13,9 +13,6 @@ const MyNavBar = () => {
             <Menu.Item key="home">
                 <Link to ="/" exact="true">HOME</Link>
             </Menu.Item>
-            <Menu.Item key = "test">
-                <Link to ="/test" exact="true">TEST</Link>
-            </Menu.Item>
             <SubMenu title ="JOBS">
                 <Menu.Item key ="jobs">
                     <Link to ="/jobs" exact="true">Jobs List</Link>
