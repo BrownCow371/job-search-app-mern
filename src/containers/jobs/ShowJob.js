6209547e021bd7a978db5de77f77e5e644fe9f70
BrownCow371 <jmbrowncow@gmail.com>
2019-02-25 17:52:23 -0800

    add logic to redirect if job is not available and params id is not new

--- src/containers/jobs/ShowJob.js
+++ src/containers/jobs/ShowJob.js
@@ -1,19 +1,24 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux'
 import JobDetails from '../../components/jobs/JobDetails.js'
+import {Redirect} from 'react-router-dom';
 
 class ShowJob extends Component {
 
     render(){
         let id = this.props.match.params.id;
         let job = this.props.jobs.find(job => job._id === id)
-        return (
-            <div>
-                <JobDetails
-                    job={job}
-                />
-            </div>
-    )}
+        if (!job && this.props.match.params.id !=='new'){
+            return <Redirect to="/"/>
+        } else {
+            return (
+                <div>
+                    <JobDetails
+                        job={job}
+                    />
+                </div>
+            )}
+        }
 }
 const mapStateToProps = (state) => {
     return {jobs: state.jobs}
